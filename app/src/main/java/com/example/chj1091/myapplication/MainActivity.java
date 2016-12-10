ced3096b67b2c4203fe8d427e7c2eb846e189f8d
yangzhanshan <yangzhanshan@chehejia.com>
2016-12-10 23:00:11 +0800

    update java,and test push.

--- app/src/main/java/com/example/chj1091/myapplication/MainActivity.java
+++ app/src/main/java/com/example/chj1091/myapplication/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+	//添加push 1.
     }
 }
